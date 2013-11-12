c74b8550aa817983b4d95c176cea1d4f29795d86
Tad <tad0616@gmail.com>
2013-11-12 16:08:46 +0800

    新增專案

---
+++ function.php
@@ -0,0 +1,55 @@
+<?php
+//引入TadTools的函式庫
+if(!file_exists(XOOPS_ROOT_PATH."/modules/tadtools/tad_function.php")){
+ redirect_header("http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50",3, _TAD_NEED_TADTOOLS);
+}
+include_once XOOPS_ROOT_PATH."/modules/tadtools/tad_function.php";
+
+define("_TAD_ASSIGNMENT_UPLOAD_DIR",XOOPS_ROOT_PATH."/uploads/tad_assignment/");
+define("_TAD_ASSIGNMENT_UPLOAD_URL",XOOPS_URL."/uploads/tad_assignment/");
+
+
+//以流水號取得某筆tad_assignment資料
+function get_tad_assignment($assn=""){
+	global $xoopsDB;
+	if(empty($assn))return;
+	$sql = "select * from ".$xoopsDB->prefix("tad_assignment")." where assn='$assn'";
+	$result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());
+	$data=$xoopsDB->fetchArray($result);
+	return $data;
+}
+
+
+//轉換成時間戳記
+function day2ts($day="",$sy="-"){
+	if(empty($day))$day=date("Y-m-d H:i:s");
+	$dt=explode(" ",$day);
+
+	$d=explode($sy,$dt[0]);
+	$t=explode(":",$dt[1]);
+
+	$ts=mktime($t['0'],$t['1'],$t['2'],$d['1'],$d['2'],$d['0']);
+	return $ts;
+}
+/********************* 預設函數 *********************/
+//圓角文字框
+function div_3d($title="",$main="",$kind="raised",$style="",$other=""){
+
+	$p=(!empty($other))?"<p style='border:1px dotted gray;padding:8px;'>$other</p>":"";
+	$main="<table style='width:auto;{$style}'><tr><td>
+	<div class='{$kind}'>
+	<h1>$title</h1>
+	$p
+	<b class='b1'></b><b class='b2'></b><b class='b3'></b><b class='b4'></b>
+	<div class='boxcontent'>
+ 	$main
+	</div>
+	<b class='b4b'></b><b class='b3b'></b><b class='b2b'></b><b class='b1b'></b>
+	</div>
+	</td></tr></table>";
+	return $main;
+}
+
+
+
+?>
---
+++ language/tchinese_utf8/modinfo.php
@@ -0,0 +1,17 @@
+<?php
+include_once XOOPS_ROOT_PATH."/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+define("_MI_TADASSIGN_NAME","上傳模組");
+define("_MI_TADASSIGN_AUTHOR","上傳模組");
+define("_MI_TADASSIGN_CREDITS","tad");
+define("_MI_TADASSIGN_DESC","此模組可以讓管理者（老師）開設需上傳檔案的項目，以便讓使用者（學生）上傳檔案。");
+define("_MI_TADASSIGN_ADMENU1", "管理主題");
+define("_MI_TADASSIGN_ADMENU2", "新增上傳主題");
+define("_MI_TADASSIGN_ADMENU3", "新增檔案格式");
+define("_MI_TADASSIGN_TEMPLATE_DESC1", "index_tpl.html的樣板檔。");
+define("_MI_TADASSIGN_TEMPLATE_DESC2", "show_tpl.html的樣板檔。");
+define("_MI_TADASSIGN_BNAME1","上傳");
+define("_MI_TADASSIGN_BDESC1","列出目前開放上傳的項目");
+define("_MI_TADASSIGN_BNAME2","優秀作品展示區");
+define("_MI_TADASSIGN_BDESC2","會依照成績排序來取前幾名秀出");
+
+?>
