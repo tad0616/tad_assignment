c0d6828be9c091d1ea8cdfc35bdeb9980ee47118
Tad <tad0616@gmail.com>
2014-01-29 13:54:41 +0800

    精簡不必要函數

--- function.php
+++ function.php
@@ -11,45 +11,27 @@ define("_TAD_ASSIGNMENT_UPLOAD_URL",XOOPS_URL."/uploads/tad_assignment/");
 
 //以流水號取得某筆tad_assignment資料
 function get_tad_assignment($assn=""){
-	global $xoopsDB;
-	if(empty($assn))return;
-	$sql = "select * from ".$xoopsDB->prefix("tad_assignment")." where assn='$assn'";
-	$result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());
-	$data=$xoopsDB->fetchArray($result);
-	return $data;
+  global $xoopsDB;
+  if(empty($assn))return;
+  $sql = "select * from ".$xoopsDB->prefix("tad_assignment")." where assn='$assn'";
+  $result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());
+  $data=$xoopsDB->fetchArray($result);
+  return $data;
 }
 
 
 //轉換成時間戳記
 function day2ts($day="",$sy="-"){
-	if(empty($day))$day=date("Y-m-d H:i:s");
-	$dt=explode(" ",$day);
+  if(empty($day))$day=date("Y-m-d H:i:s");
+  $dt=explode(" ",$day);
 
-	$d=explode($sy,$dt[0]);
-	$t=explode(":",$dt[1]);
+  $d=explode($sy,$dt[0]);
+  $t=explode(":",$dt[1]);
 
-	$ts=mktime($t['0'],$t['1'],$t['2'],$d['1'],$d['2'],$d['0']);
-	return $ts;
+  $ts=mktime($t['0'],$t['1'],$t['2'],$d['1'],$d['2'],$d['0']);
+  return $ts;
 }
 /********************* 預設函數 *********************/
-//圓角文字框
-function div_3d($title="",$main="",$kind="raised",$style="",$other=""){
-
-	$p=(!empty($other))?"<p style='border:1px dotted gray;padding:8px;'>$other</p>":"";
-	$main="<table style='width:auto;{$style}'><tr><td>
-	<div class='{$kind}'>
-	<h1>$title</h1>
-	$p
-	<b class='b1'></b><b class='b2'></b><b class='b3'></b><b class='b4'></b>
-	<div class='boxcontent'>
- 	$main
-	</div>
-	<b class='b4b'></b><b class='b3b'></b><b class='b2b'></b><b class='b1b'></b>
-	</div>
-	</td></tr></table>";
-	return $main;
-}
-
 
 
 ?>
