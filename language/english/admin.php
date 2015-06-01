67966b611af606813c55671ffbfd93582cc89b87
mambax7 <mambax7@gmail.com>
2015-06-01 02:52:16 -0400

    Adding English files

---
+++ language/english/admin.php
@@ -0,0 +1,20 @@
+<?php
+include_once "../../tadtools/language/{$xoopsConfig['language']}/admin_common.php";
+define('_TAD_NEED_TADTOOLS', "This module needs TadTools module. You can download TadTools from <a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad's web</a>.");
+
+define('_MA_INPUT_FORM', 'build Upload subject');
+
+define('_MA_TADASSIGN_ASSN_LIST', 'Upload the theme list');
+define('_MA_TADASSIGN_ASSN', 'Upload number');
+define('_MA_TADASSIGN_TITLE', 'Upload subject');
+define('_MA_TADASSIGN_PASSWD', 'Upload password');
+define('_MA_TADASSIGN_PASSWD_DESC', '(If you enter a password, user will be able to upload files without login)');
+define('_MA_TADASSIGN_START_DATE', 'Start uploading time');
+define('_MA_TADASSIGN_END_DATE', 'Upload Deadline');
+define('_MA_TADASSIGN_NOTE', 'Upload Help');
+define('_MA_TADASSIGN_UID', 'Creator');
+define('_MA_TADASSIGN_SHOW', 'Showing upload files');
+define('_MA_TADASSIGN_SHOW_ASSIGNMENT_YES', 'that other people can view');
+define('_MA_TADASSIGN_SHOW_ASSIGNMENT_NO', 'No, don\' show it to others');
+define('_MA_TADASSIGN_ADD_TYPE_TITLE', 'Select the file format to be added');
+define('_MA_TADASSIGN_TYPE_LIST', 'Currently available upload format');
---
+++ language/english/blocks.php
@@ -0,0 +1,5 @@
+<?php
+
+define('_MB_TADASSIGN_TAD_GOOD_ASSIGNMENT_EDIT_BITEM0', 'which showed off an upload Project');
+define('_MB_TADASSIGN_TAD_GOOD_ASSIGNMENT_EDIT_BITEM1', 'take the top few?');
+define('_MB_TADASSIGN_SELECT_ASSN', 'choose Upload theme');
