1340b6c1a9b584b547530db77f4fdbfbd9522940
mambax7 <mambax7@gmail.com>
2019-04-13 08:07:16 -0400

    mkdir race condition
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- class/Utility.php
+++ class/Utility.php
@@ -213,7 +213,9 @@ PRIMARY KEY ( `type` )
         if (!is_dir($dir)) {
             umask(000);
             //若建立失敗秀出警告訊息
-            mkdir($dir, 0777);
+            if (!mkdir($dir, 0777) && !is_dir($dir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+            }
         }
     }
 
@@ -222,7 +224,9 @@ PRIMARY KEY ( `type` )
     public static function full_copy($source = '', $target = '')
     {
         if (is_dir($source)) {
-            @mkdir($target);
+            if (!mkdir($target) && !is_dir($target)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $target));
+            }
             $d = dir($source);
             while (false !== ($entry = $d->read())) {
                 if ('.' == $entry || '..' == $entry) {
