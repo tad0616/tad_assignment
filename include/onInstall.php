90d77a0531c43bffbfe037c97ebd2b485c53cf03
Tad <tad0616@gmail.com>
2019-05-10 15:35:37 +0800

    更新說明

--- include/onInstall.php
+++ include/onInstall.php
@@ -1,7 +1,9 @@
 <?php
 
 use XoopsModules\Tadtools\Utility;
-
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
 function xoops_module_install_tad_assignment(&$module)
 {
     Utility::mk_dir(XOOPS_ROOT_PATH . '/uploads/tad_assignment');
--- xoops_version.php
+++ xoops_version.php
@@ -3,7 +3,7 @@ $modversion = [];
 
 //---模組基本資訊---//
 $modversion['name'] = _MI_TADASSIGN_NAME;
-$modversion['version'] = 2.51;
+$modversion['version'] = 2.52;
 $modversion['description'] = _MI_TADASSIGN_DESC;
 $modversion['author'] = _MI_TADASSIGN_AUTHOR;
 $modversion['credits'] = _MI_TADASSIGN_CREDITS;
@@ -14,7 +14,7 @@ $modversion['image'] = "images/logo_{$xoopsConfig['language']}.png";
 $modversion['dirname'] = basename(__DIR__);
 
 //---模組狀態資訊---//
-$modversion['release_date'] = '2019-01-01';
+$modversion['release_date'] = '2019-05-10';
 $modversion['module_website_url'] = 'https://tad0616.net/';
 $modversion['module_website_name'] = _MI_TAD_WEB;
 $modversion['module_status'] = 'release';
