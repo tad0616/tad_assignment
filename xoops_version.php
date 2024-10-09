6e1c75269e0b4b915a54a2a0b3819930ca12b4a4
tad0616 <tad0616@gmail.com>
2024-10-09 16:40:48 +0800

    簡化paypal資訊

--- xoops_version.php
+++ xoops_version.php
@@ -27,11 +27,12 @@ $modversion['min_xoops'] = '2.5';
 $modversion['min_tadtools'] = '2.02';
 
 //---paypal資訊---//
-$modversion['paypal'] = [];
-$modversion['paypal']['business'] = 'tad0616@gmail.com';
-$modversion['paypal']['item_name'] = 'Donation : ' . _MI_TAD_WEB;
-$modversion['paypal']['amount'] = 0;
-$modversion['paypal']['currency_code'] = 'USD';
+$modversion['paypal'] = [
+    'business' => 'tad0616@gmail.com',
+    'item_name' => 'Donation : ' . _MI_TAD_WEB,
+    'amount' => 0,
+    'currency_code' => 'USD',
+];
 
 //---資料表架構---//
 $modversion['sqlfile']['mysql'] = 'sql/mysql.sql';
