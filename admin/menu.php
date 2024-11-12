bd400adbb6462a567c2a084c7c61e794470d39f9
tad0616 <tad0616@gmail.com>
2024-11-12 16:20:06 +0800

    簡化後台選單陣列，修改釋出日期

--- admin/menu.php
+++ admin/menu.php
@@ -1,27 +1,6 @@
 <?php
-$adminmenu = [];
-$icon_dir = '2.6' === mb_substr(XOOPS_VERSION, 6, 3) ? '' : 'images/';
-
-$i = 1;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_HOME;
-$adminmenu[$i]['link'] = 'admin/index.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_HOME_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/home.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ASSIGNMENT_ADMENU1;
-$adminmenu[$i]['link'] = 'admin/main.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ASSIGNMENT_ADMENU1;
-$adminmenu[$i]['icon'] = 'images/admin/folder.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ASSIGNMENT_ADMENU3;
-$adminmenu[$i]['link'] = 'admin/add_type.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ASSIGNMENT_ADMENU3;
-$adminmenu[$i]['icon'] = 'images/admin/type.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_ABOUT;
-$adminmenu[$i]['link'] = 'admin/about.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_ABOUT_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/about.png';
+$adminmenu = [
+    ['title' => _MI_TAD_ASSIGNMENT_ADMENU1, 'link' => 'admin/main.php', 'desc' => _MI_TAD_ASSIGNMENT_ADMENU1, 'icon' => 'images/admin/folder.png'],
+    ['title' => _MI_TAD_ASSIGNMENT_ADMENU3, 'link' => 'admin/add_type.php', 'desc' => _MI_TAD_ASSIGNMENT_ADMENU3, 'icon' => 'images/admin/type.png'],
+    ['title' => _MI_TAD_ADMIN_ABOUT, 'link' => 'admin/about.php', 'desc' => _MI_TAD_ADMIN_ABOUT_DESC, 'icon' => 'images/admin/about.png'],
+];
